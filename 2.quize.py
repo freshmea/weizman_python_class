05697792e311301ebfe544421ea2cb388c024d6f
freshmea <freshmea@gmail.com>
2021-03-21 01:26:31 +0900

    퀴즈에 시간초과 넣기

--- 2.quize.py
+++ 2.quize.py
@@ -1,24 +1,25 @@
 import time
 import threading
 
+"""input을 실행하면서 시간제한을 알려주는 함수"""
+def input_timeout(timeout=4.0):
+    timer = threading.Timer(timeout, overtime)
+    timer.start()
+    astring=input()
+    timer.cancel()
+    return astring
 
+"""input_timeout에 의해 실행되고 시간이초과되면 실행되는 함수"""
 def overtime():
     global check
     print('시간초과')
     check=1
     pass
 
-def input_timeout(timeout=4.0):
-    timer = threading.Timer(timeout, overtime)
-    astring = 0
-    timer.start()
-    astring = input()
-    timer.cancel()
-    return astring
-
+"""정답과 오답을 알려주는 함수"""
 def yesorno(a=0):
     global score
-    a1=int(input_timeout(timeout=4.0))
+    a1=input_timeout(timeout=4.0)
     if a1==a and check==0:
         print('정답입니다!!!!')
         score = score + 1
@@ -34,7 +35,7 @@ def yesorno(a=0):
 score = 0
 q1=['13+24*2 는 무엇일까요?', '파이 는 무엇일까요?', '파이썬에서 콘솔에 문자를 출력하는 명령어는 무엇일까요?',
     '다음중 물질의 상태가 아닌 것은 무엇 인가요?']
-q2=[3, 2, 1, 3]
+q2=['3', '2', '1', '3']
 q3=[[0, 10, 61, 71], [4.23, 3.14, 5.13, 3.15], ['print', 'input', 'get', 'if else'], ['기체', '고체', '물체', '액체']]
 
 
