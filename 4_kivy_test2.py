e8068e2f192be4b723139d70c224f0299d0523ea
freshmea <freshmea@gmail.com>
2021-03-29 03:18:16 +0900

    음악파일 프로세싱

--- 4_kivy_test2.py
+++ 4_kivy_test2.py
@@ -6,6 +6,14 @@ from kivy.clock import Clock
 from random import randint
 
 
+class PongPaddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            ball.velocity_x *= -1.1
+
+
 class PongBall(Widget):
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
@@ -15,8 +23,9 @@ class PongBall(Widget):
         self.pos = Vector(*self.velocity)+self.pos
 
 class PongGame(Widget):
-
     ball = ObjectProperty(None)
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
 
     def serve_ball(self):
         self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
@@ -24,13 +33,32 @@ class PongGame(Widget):
     def update(self, dt):
         self.ball.move()
 
+        if (self.ball.y < 0) or (self.ball.y > self.height-50):
+            self.ball.velocity_y *= -1
+
+        if self.ball.x < 0:
+            self.ball.velocity_x *= -1
+            self.player1.score += 1
+
+        if self.ball.x > self.width-50 :
+            self.ball.velocity_x *= -1
+            self.player2.score += 1
+
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
+
+    def on_touch_move(self, touch):
+        if touch.x<self.width / 1/4:
+            self.player1.center_y = touch.y
+        if touch.x> self.width * 3/4:
+            self.player2.center_y = touch.y
 
 
-class PongaApp(App):
+class PongApp(App):
     def build(self):
         game = PongGame()
         game.serve_ball()
         Clock.schedule_interval(game.update,1.0/60.0)
         return game
 
-PongaApp().run()
+PongApp().run()
