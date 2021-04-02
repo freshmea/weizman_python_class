e646ffc15a36e0e0f1c9daa102e1303df53ca69c
freshmea <freshmea@naver.com>
2021-04-02 19:02:35 +0900

    4차시 거북이 가지고 그리기

--- 1-4_turtle.py
+++ 1-4_turtle.py
@@ -1,11 +1,42 @@
-import turtle
+from turtle import forward as 앞으로
+from turtle import backward as 뒤로
+from turtle import mainloop as 그대로두세요
+from turtle import left as 왼쪽
+from turtle import right as 오른쪽
+from turtle import penup as 그만그려
+from turtle import pendown as 그려
+from turtle import pensize as 굵기
+from turtle import speed as 속도
+from turtle import pencolor as 색
+from turtle import clear as 지우기
 
-turtle.color('red', 'yellow')
-turtle.begin_fill()
-while True:
-    turtle.forward(200)
-    turtle.left(170)
-    if abs(turtle.pos())<1:
-        break
-turtle.end_fill()
-turtle.done()
+
+# 색의 종류 red, orange, yellow, green, blue, indigo, violet, d
+# 속도(1)
+# 그만그려()
+# 왼쪽(90)
+# 앞으로(200)
+# 왼쪽(90)
+# 앞으로(100)
+# 오른쪽(180)
+# 색('yellow')
+# 그려()
+# 속도(5)
+# k=0
+# for i in range(10):
+#     k=k+1
+#     앞으로(100)
+#     왼쪽(10)
+#     굵기(k)
+#     뒤로(200)
+#     왼쪽(10)
+# 지우기()
+# 그대로두세요()
+import turtle
+turtle.shape("turtle")
+앞으로(100)
+오른쪽(90)
+앞으로(100)
+오른쪽(90)
+앞으로(100)
+
