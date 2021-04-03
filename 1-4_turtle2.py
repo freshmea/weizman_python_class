e2662c254652f293ac8c7c5d9a7bf193cf919b5b
freshmea <freshmea@naver.com>
2021-04-03 14:48:22 +0900

    터틀

--- 1-4_turtle2.py
+++ 1-4_turtle2.py
@@ -10,9 +10,31 @@ from turtle import speed as 속도
 from turtle import pencolor as 색
 from turtle import clear as 지우기
 from turtle import goto as 이동
+from turtle import circle as 원
 
 import random
 import turtle
 turtle.shape("turtle")
+import winsound
 
 # 색의 종류 red, orange, yellow, green, blue, indigo, violet
+def 사각형():
+    앞으로(100)
+    왼쪽(90)
+    앞으로(100)
+    왼쪽(90)
+    앞으로(100)
+    왼쪽(90)
+    앞으로(100)
+    return
+# winsound.Beep(500, 2000)
+# for i in range(10):
+#     사각형()
+#     왼쪽(10)
+
+
+
+원(100)
+사각형()
+turtle.onscreenclick(turtle.goto)
+그대로두세요()
