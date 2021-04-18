6010fbb7afecf39ada83d2cb8b84b02247aab96e
freshmea <freshmea@gmail.com>
2021-04-18 14:48:15 +0900

    2-3 자료

--- 2-3_dict_tuple_set.py
+++ 2-3_dict_tuple_set.py
@@ -19,5 +19,34 @@ print(person['country'])    # Finland
 print(person['skills'])     # ['JavaScript', 'React', 'Node', 'MongoDB', 'Python']
 print(person['skills'][0])  # JavaScript
 print(person['address']['street']) # Space street
-print(person['city'])       # Error
+#print(person['city'])       # Error
 
+
+"""튜플을 연습해 봅시다."""
+
+# 구문으로 정의 하기
+empty_tuple = ()
+# 튜플 클래스로 정의 하기
+empty_tuple = tuple()
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+fruits = list(fruits)
+fruits[0] = 'apple'
+print(fruits)     # ['apple', 'orange', 'mango', 'lemon']
+fruits = tuple(fruits)
+print(fruits)     # ('apple', 'orange', 'mango', 'lemon')
+
+
+
+"""셋을 연습해 봅시다. """
+
+# 구문으로 정의 하기
+st = {}
+# 셋 클래스로 정의 하기
+st = set()
+
+#포함 관계 확인 하기
+st1 = {'item1', 'item2', 'item3', 'item4'}
+st2 = {'item2', 'item3'}
+st2.issubset(st1) # True
+st1.issuperset(st2) # True
