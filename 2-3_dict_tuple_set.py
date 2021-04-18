ebc4ee7d20abf034cbf42515fa63bab8f6ab6c51
freshmea <freshmea@gmail.com>
2021-04-18 09:20:56 +0900

    2-3 파일

---
+++ 2-3_dict_tuple_set.py
@@ -0,0 +1,23 @@
+person = {
+    'first_name':'Asabeneh',
+    'last_name':'Yetayeh',
+    'age':250,
+    'country':'Finland',
+    'is_marred':True,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+    }
+
+print(len(person))
+
+
+print(person['first_name']) # Asabeneh
+print(person['country'])    # Finland
+print(person['skills'])     # ['JavaScript', 'React', 'Node', 'MongoDB', 'Python']
+print(person['skills'][0])  # JavaScript
+print(person['address']['street']) # Space street
+print(person['city'])       # Error
+
