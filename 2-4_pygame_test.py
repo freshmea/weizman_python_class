34e2b011abc289a201384bd6259d98cee2f4006a
freshmea <freshmea@naver.com>
2021-05-01 15:00:52 +0900

    2-4 수업 수정

---
+++ 2-4_pygame_test.py
@@ -0,0 +1,31 @@
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+clock = pygame.time.Clock()
+a_image = pygame.image.load('images/ben-a.svg')
+font = pygame.font.SysFont('malgungothic', 36)
+
+y = 0
+x = 0
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    clock.tick(60)
+    pressed_keys = pygame.key.get_pressed()
+    if pressed_keys[pygame.K_w]:
+        y -= 1
+    if pressed_keys[pygame.K_s]:
+        y += 1
+    if pressed_keys[pygame.K_a]:
+        x -= 1
+    if pressed_keys[pygame.K_d]:
+        x += 1
+    screen.fill((255,255,255))
+    screen.blit(a_image, (x ,y))
+    a=font.render('안녕하세요!!!!', True, (255, 0, 0))
+    screen.blit(a, (0,0))
+
+    pygame.display.update()
--- 과제2-3_1.py
+++ 과제2-3_1.py
@@ -7,7 +7,7 @@ def between_markers(text: str, begin: str, end: str) -> str:
         returns substring between two given markers
     """
     # your code here
-    return
+    return text[text.index(begin)+1:text.index(end)]
 
 
 if __name__ == '__main__':
