e0a445a85638a4d1e3621c17a357f6741c463f65
freshmea <freshmea@gmail.com>
2021-05-04 11:42:23 +0900

    3-1

--- test.py
+++ test.py
@@ -1,22 +1,25 @@
-# import random
-#
-#
-# class Cuckie: #항상 클래스는 대문자로 쓴다.
-#     def __init__(self):
-#         self.x = random.randint(0,100)
-#     def burn(self):
-#         print("쿠키가 구워졌어요.")
-#
-# cuckie1 = Cuckie()
-# cuckie2 = Cuckie()
-# cuckie3 = Cuckie()
-# cuckie4 = Cuckie()
-# print(cuckie1.x)
-# print(cuckie2.x)
-# print(cuckie3.x)
-# print(cuckie4.x)
-#
-#
-# cuckie1 = Cuckie()
-# cuckie3 = Cuckie()
-#
+
+
+
+class Rgb:
+    def __init__(self, r, g, b):
+        self.r =r
+        self.g =g
+        self.b =b
+
+    def __add__(self, other):
+        self.r += other.r
+        self.g += other.g
+        self.b += other.b
+
+
+    def pr(self):
+        print(self.r, self.g, self.b)
+
+a=Rgb(1,2,3)
+b=Rgb(23,13,13)
+
+c=a+b
+a.pr()
+b.pr()
+print(type(c))
