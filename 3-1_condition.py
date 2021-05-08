4f18eb3a6d2e7f605e6fb04ce3d5d8a197ada8d7
freshmea <freshmea@naver.com>
2021-05-08 15:09:03 +0900

    3-1 수업 수정

--- 3-1_condition.py
+++ 3-1_condition.py
@@ -1,7 +1,33 @@
 a=0
 for i in range(1000):
     a +=1
-    print(a, end='  ')
+    if a%3 == 0 or a%5 == 0:
+        print(a, end='  ')
+        if a%30 == 0:
+            print()
 
 
-a=input('숫자를 입력하시오: ')
+#a=input('숫자를 입력하시오: ')
+
+# 산술 연산
+a = 10
+b = 3
+print('a + b = ', a + b)
+print('a - b = ', a - b)
+print('a * b = ', a * b)
+print('a / b = ', a / b)
+print('a % b = ', a % b)
+print('a // b = ', a // b)
+print('a ** b = ', a ** b)
+
+# 관계 연산자
+a = 10
+b = 3
+print(a > b)
+print(a >= b)
+print(a < b)
+print(a > b)
+print(a >= b)
+print(a == b)
+print(a != b)
+
