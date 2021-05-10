438b11a398810c7adc09da05ed1a6d6edb11a86b
freshmea <freshmea@naver.com>
2021-05-10 21:22:14 +0900

    3-1 수업 마지막 문제

---
+++ 3-1_condition_마지막 문제.py
@@ -0,0 +1,16 @@
+
+a=input('숫자나 소수, 문자열을 입력하세요:')
+
+if a.isalpha():
+    print('입력한 값은 문자열')
+elif '.' in a:
+    a=a.replace('.', '0', 1)
+    if a.isdigit():
+        print('입력한 값은 소수')
+    else:
+        print('문자열도 소수도 정수도 아님')
+else:
+    if a.isdigit():
+        print('입력한 값은 정수')
+    else:
+        print('문자열도 소수도 정수도 아님')
