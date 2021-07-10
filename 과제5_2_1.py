f2e28bce25a70d27a823181029b9431fd59408dc
freshmea <freshmea@naver.com>
2021-07-10 15:46:23 +0900

    5-2 숙제

--- 과제5_2_1.py
+++ 과제5_2_1.py
@@ -1,4 +1,10 @@
-# 파이게임 비 내리는 코드(클래스)
+# 구름 클래스 추가
+# 비가 사선으로 내리기
+# 구름 클릭하면 구름 제거 하기
+# images 안의 back.png 라는 배경 파일도 같이 있어야 실행이 됩니다.
+# images 안의 dino.png 라는 공룡 파일도 같이 있어야 실행이 됩니다.
+# images 안의 cloud.svg 라는 구름 파일도 같이 있어야 실행이 됩니다.
+
 import pygame
 import random
 
@@ -6,6 +12,7 @@ import random
 SCREEN_X = 640 * 2  # 화면 넓이
 SCREEN_Y = 480 * 2  # 화면 높이
 FPS = 60
+CLOUD_NUMBER = 5
 
 
 class Cloud:
@@ -36,14 +43,14 @@ class Cloud:
 
 
 class Rain:
-    def __init__(self, x, y, game):
+    def __init__(self, x, y, root):
         self.x = x
         self.y = y
         self.speed = random.randint(5, 28)
         self.bold = random.randint(1, 4)
-        self.game = game
+        self.game = root
         self.len = random.randint(5, 15)
-        self.color = pygame.Color('skyblue')
+        self.color = pygame.Color('gray')
 
     def move(self):
         self.y += self.speed
@@ -56,6 +63,27 @@ class Rain:
         pygame.draw.line(self.game.screen, self.color, (self.x, self.y), (self.x, self.y + self.len), self.bold)
 
 
+class Dino:
+    def __init__(self, root):
+        self.game = root
+        self.image = self.game.image_dino
+        self.x = SCREEN_X / 2
+        self.y = SCREEN_Y - self.image.get_rect().height
+        self.speed = 4
+
+    def load_data(self):
+        pass
+
+    def move(self):
+        self.x += self.speed
+
+    def off_screen(self):
+        return self.x < -self.image.get_rect().width or self.x > SCREEN_X
+
+    def draw(self):
+        self.game.screen.blit(self.image, (self.x, self.y))
+
+
 class Game:
     def __init__(self):
         pygame.init()
@@ -63,13 +91,16 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_X, SCREEN_Y))  # 화면 세팅
         self.clock = pygame.time.Clock()  # 시계 지정
         self.playing = True
+        self.load_data()
         self.rains = []
         self.clouds = []
-        self.load_data()
+        self.dino = Dino(self)
 
     def load_data(self):
         self.image = pygame.image.load('images/back.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (SCREEN_X, SCREEN_Y))
+        self.image_dino = pygame.image.load('images/dino.png').convert_alpha()
+        self.image_dino = pygame.transform.scale(self.image_dino, (260,200))
         self.image_cloud = pygame.image.load('images/cloud.svg').convert_alpha()
 
     def run(self):
@@ -84,6 +115,7 @@ class Game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.playing = False
+            # 마우스 버튼이 구름 클릭시 구름 제거
             if event.type == pygame.MOUSEBUTTONDOWN:
                 for cloud in self.clouds:
                     if cloud.click():
@@ -92,7 +124,7 @@ class Game:
 
     def update(self):
         # 구름 생성
-        while len(self.clouds) < 10:
+        while len(self.clouds) < CLOUD_NUMBER:
             self.clouds.append(Cloud(random.randint(0, SCREEN_X), self))
         # 구름에서 비 내리기
         for cloud in self.clouds:
@@ -106,14 +138,24 @@ class Game:
         # 구름 움직이기
         for cloud in self.clouds:
             cloud.move()
+        # 공룡 움직이기
+        self.dino.move()
+        if self.dino.off_screen():
+            self.dino.speed *= -1
+            self.dino.image = pygame.transform.flip(self.dino.image, True, False)
 
     def draw(self):
         self.screen.fill((255, 255, 255))
+        # 배경화면
         self.screen.blit(self.image, (0, 0))
+        # 비 그리기
         for rain in self.rains:
             rain.draw()
+        # 구름 그리기
         for cloud in self.clouds:
             cloud.draw()
+        # 공룡 그리기
+        self.dino.draw()
 
 
 game = Game()
